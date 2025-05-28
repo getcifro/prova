94d114c37382ed8909c9dc59f9658da657c83fbb
getcifro <getcifro@gmail.com>
2025-05-28 12:54:33 -0300

    ok

---
+++ debian_auto.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+# Copyright (c) 2025
+# Author: Ramon Silas
+# Prova de SO Livres
+
+clear
+cat <<"EOF"
+  ____      _    __  __  ___  _   _ 
+ |  _ \    / \  |  \/  |/ _ \| \ | |
+ | |_) |  / _ \ | |\/| | | | |  \| |
+ |  _ <  / ___ \| |  | | |_| | |\  |
+ |_| \_\/_/   \_\_|  |_|\___/|_| \_|
+                                    
+EOF
+
+echo -e "Iniciando configuração..."
+
+#Configuração do  repositorio
+# /etc/apt/sources.list.d/debian.sources
+
+# Copia de segurança
+cp /etc/apt/sources.list.d/debian.sources /etc/apt/sources.list.d/debian.sources.copy
+
+#comentando linha do CD/ROM
+
+sed -i '1,3s/^[^#]/# &/g' /etc/apt/sources.list.d/debian.sources
+
+#Adicionando repositorios
+
+echo "deb http://deb.debian.org/debian bookworm main non-free-firmware" >> /etc/apt/sources.list.d/debian.sources"
+echo "deb-src http://deb.debian.org/debian bookworm main non-free-firmware" >> /etc/apt/sources.list.d/debian.sources"
+
+echo "deb http://deb.debian.org/debian-security/ bookworm-security main non-free-firmware" >> /etc/apt/sources.list.d/debian.sources"d
+echo "deb-src http://deb.debian.org/debian-security/ bookworm-security main non-free-firmware" >> /etc/apt/sources.list.d/debian.sources"
+
+echo "deb http://deb.debian.org/debian bookworm-updates main non-free-firmware" >> /etc/apt/sources.list.d/debian.sources"
+echo "ddeb-src http://deb.debian.org/debian bookworm-updates main non-free-firmware" >> /etc/apt/sources.list.d/debian.sources"
+
+echo Arquivos de repositios configurado!
+
+
+#Atualização
+
+#Atualizando repositorios e sistema
+apt-get -y update && sudo apt full-upgrade
+
+
+#Cmatrix
+#Instação do CMTRIX
+apt -y install cmatrix
+
+#Cmatrix
+#Instação do CMTRIX
+apt -y install cmatrix
+
+echo -e CMTRIX instalado!
+
+
+#SSH
+#Instação do SSH
+apt -y install cmatrix
+echo -e SSH instalado!
+
+msg_ok "Configuração completa!\n"
+echo -e "Para acessar ssh.
+         ${BL}http://${IP}:22${CL} \n"
